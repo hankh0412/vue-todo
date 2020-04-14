386b854c1fd9f732ecb2eb44c06dbe7a8dc6c7b4
hankh0412 <hankh0412@gmail.com>
2020-04-14 14:11:12 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+      <TodoHeader></TodoHeader>
+      <TodoFooter></TodoFooter>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+    </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoFooter': TodoFooter,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList
+  }
+    
+}
+</script>
+
+<style>
+</style>
